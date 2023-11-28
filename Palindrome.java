74ba2c2a4cedd9b68afdf22c498e764e05ed5bc9
Sjanrie S Hendrikz <108018913+19SJah22@users.noreply.github.com>
2023-11-28 15:41:39 +0200

    Add files via upload

---
+++ Palindrome.java
@@ -0,0 +1,55 @@
+/*
+ * Group:
+ * Sjanrie Hendrikz 20230667
+ * Ihtishaam Isaacs 20232354
+ * Mornay Schoombie 20230775
+ * Gareth Bekker 20230758
+ */
+
+package com.mycompany.palindromesjanriehendrikz;
+
+import java.util.Scanner;
+
+public class PalindromeSjanrieHendrikz {
+
+    public static void main(String[] args) {
+         Scanner input = new Scanner(System.in);
+       
+        System.out.println("-----------------------------------------------------------------------");
+        System.out.println(" ");
+        System.out.println("Please give the input number to check palindrome: ");
+        System.out.println(" ");
+        String str = input.nextLine();
+        System.out.println(" ");
+        System.out.println("-----------------------------------------------------------------------");
+        System.out.println(" ");
+        
+        long num = Long.parseLong(str);
+        
+        boolean isPalindrome = true;
+        long reverse = 0;
+        long temp = num;
+        
+        while(temp !=0) {
+            reverse = reverse * 10 + temp% 10;
+            temp/= 10;
+        }    
+        if  (num != reverse) {
+            isPalindrome = false;
+        }
+        
+        System.out.println("Input By User: " + str);
+        System.out.println(" ");
+        System.out.println("Reverse number: " + reverse);
+        System.out.println(" ");
+        
+        if (isPalindrome){
+            System.out.println(str + " is a Palindrome Number");
+            System.out.println(" ");
+        } 
+        else {
+           System.out.println(str + " is not a Palindrome Number"); 
+           System.out.println(" ");
+        }    
+    }
+}
